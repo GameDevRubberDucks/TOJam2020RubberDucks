dfab455a816cb7a8fa520dacd95e4a60c12e57e1
Dan <dmaccormick@hotmail.com>
2020-05-08 11:19:00 -0400

    [ADD] Created the basics of the call manager and call groups
    - Not fully implemented
    - Created some useful enums and event types to help with managing info
    - Set up the skeleton for spawning new calls and handling when they disconnect

---
+++ TOJam2020/Assets/Studio Assets/Scripts/Call_Group.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Call_Group
+{
+    //--- Public Events ---/
+    public Call_CompletionEvent m_OnCallCompleted;
+
+
+
+    //--- Private Variables ---//
+    private Call_State m_callState;
+    private int m_numParticipants;
+    private float m_waitTimeMax;
+    private float m_waitTimeRemaining;
+    private float m_callTimeMax;
+    private float m_callTimeRemaining;
+
+
+
+    //--- Constructors ---//
+    public Call_Group(int _numParticipants, float _waitTimeMax, float _callTimeMax)
+    {
+        // Init the private data
+        m_callState = Call_State.Waiting;
+        m_numParticipants = _numParticipants;
+        m_waitTimeMax = _waitTimeMax;
+        m_waitTimeRemaining = m_waitTimeMax;
+        m_callTimeMax = _callTimeMax;
+        m_callTimeRemaining = m_callTimeMax;
+    }
+
+
+
+    //--- Methods ---//
+    public void UpdateCall()
+    {
+        // Update the current state depending on if individual callers have moved around
+        UpdateCallState();
+
+        // Depending on the state, handle the timer update
+        if (m_callState == Call_State.Waiting)
+        {
+            // Lower the patience meter
+            m_waitTimeRemaining -= Time.deltaTime;
+
+            // TODO: Update the UI representations of this wait time
+            // ...
+
+            // The callers waited too long and ran out of patience
+            if (m_waitTimeRemaining <= 0.0f)
+                m_OnCallCompleted.Invoke(this, Call_State.Waited_Too_Long);
+        }
+        else if (m_callState == Call_State.Active)
+        {
+            // Lower the call time
+            m_callTimeRemaining -= Time.deltaTime;
+
+            // TODO: Update the UI representations of the call time
+            // ...
+
+            // The callers completed their call successfully
+            if (m_callTimeRemaining <= 0.0f)
+                m_OnCallCompleted.Invoke(this, Call_State.Completed);
+        }
+    }
+
+    public void UpdateCallState()
+    {
+        // TODO: Check in with the individual callers and see if they are in the same room
+    }
+}
---
+++ TOJam2020/Assets/Studio Assets/Scripts/Call_HelperStructures.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.Events;
+
+//--- Helper Enums ---//
+public enum Call_State
+{
+    Waiting,                    // ANY of the call participants are disconnected
+    Active,                     // ALL of the call participants are connected TOGETHER
+    Waited_Too_Long,            // The call terminated because the participants waited too long
+    Completed                   // The call finished because the group was done their call
+}
+
+
+
+//--- Helper Events ---//
+public class Call_CompletionEvent : UnityEvent<Call_Group, Call_State> { }
