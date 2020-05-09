d9d6a6f62894cc6a97b06c6c443905eb33043ef6
Dan <dmaccormick@hotmail.com>
2020-05-09 17:42:00 -0400

    [ADD] Connected the screens to the room controls
    - The screens now turn on/off depending on if the room is active
    - The screens also split correctly according to the capacity of the room

--- TOJam2020/Assets/Studio Assets/Scripts/Room_UI.cs
+++ TOJam2020/Assets/Studio Assets/Scripts/Room_UI.cs
@@ -10,22 +10,22 @@ public class Room_UI : MonoBehaviour
 
 
 
-    //--- Methods ---//
-    public void InitWithData(Room _roomRef)
+    //--- Unity Methods ---//
+    private void Start()
     {
-        // Store the room ref
-        m_roomRef = _roomRef;
-
         // Show the UI
         UpdateUI();
     }
 
+
+
+    //--- Methods ---//
     public void UpdateUI()
     {
         // Depending on if the room is active, we should show or hide the blocker object
         m_disconnectIndicator.SetActive(!m_roomRef.IsActive);
 
         // Show the correct screen split layout for room depending on its current size
-        m_screenLayouts[m_roomRef.MaxCapacity - 1].SetActive(true);
+        m_screenLayouts[m_roomRef.MaxCapacity - 2].SetActive(true);
     }
 }
