8d3d6281765efd5fba1520f95989da5f938094f2
Dan <dmaccormick@hotmail.com>
2020-05-10 14:28:19 -0400

    [ADD] Added the code for the room UI to toggle the label
    - Commented out the line that checks for now because the UI itself is not in my build and it will throw a null reference as a result

--- TOJam2020/Assets/Studio Assets/Scripts/Room_UI.cs
+++ TOJam2020/Assets/Studio Assets/Scripts/Room_UI.cs
@@ -4,10 +4,13 @@ using UnityEngine.UI;
 public class Room_UI : MonoBehaviour
 {
     //--- Public Variables ---//
-    public GameObject m_disconnectIndicator;
+    public GameObject m_disconnectedScreen;
     public Room_UI_Layout[] m_screenLayouts;
     public GameObject m_callerUIPrefab;
     public Room m_roomRef;
+    public Image m_screenNumberLabel;
+    public Sprite m_screenNumberOn;
+    public Sprite m_screenNumberOff;
 
 
 
@@ -41,7 +44,10 @@ public class Room_UI : MonoBehaviour
     public void UpdateRoomDisplay()
     {
         // Depending on if the room is active, we should show or hide the blocker object
-        m_disconnectIndicator.SetActive(!m_roomRef.IsActive);
+        m_disconnectedScreen.SetActive(!m_roomRef.IsActive);
+
+        // Change the label light to disconnected / connected state
+        //m_screenNumberLabel.sprite = (m_roomRef.IsActive) ? m_screenNumberOn : m_screenNumberOff;
 
         // Enable the correct screen split layout for room depending on its current size
         m_activeLayout = m_screenLayouts[m_roomRef.MaxCapacity - 2];
