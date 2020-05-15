9a37b1f12253a712036f19d06e9e166f720daa1c
Bo <Wen.yu@ontariotechu.net>
2020-05-14 21:39:33 -0400

    [MODIFY]
    End of the day, end of the week, and main menu changes.

---
+++ TOJam2020/Assets/Studio Assets/Scripts/EndOfWeekManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using System;
+
+public class EndOfWeekManager : MonoBehaviour
+{
+    //public End text input
+    public TextMeshProUGUI endScreenText;
+    public float textAnimationSpeed;
+    string text;
+    int textDisplayAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textDisplayAmount = 0;
+        text = "This is a test asdlfkjal;sdj fl;askdjf;l kajsdf;lkjas dl;fkj ads;lkfhj aosdiuhfalkjsdn";
+        StartCoroutine(TypeText());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            SceneManager.LoadScene("Main Menu");
+    }
+
+
+    IEnumerator TypeText()
+    {
+        foreach (char letter in text.ToCharArray())
+        {
+            endScreenText.text += letter;
+            yield return new WaitForSeconds(textAnimationSpeed);
+        }
+    }
+}
